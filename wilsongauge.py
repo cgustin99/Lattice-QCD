013e0dd22da312b67eb7cedb0d806736949e33da
Carter M. Gustin <83713139+cgustin99@users.noreply.github.com>
2023-10-06 08:13:32 -0400

    Finished Wilson loop

--- wilsongauge.py
+++ wilsongauge.py
@@ -55,6 +55,7 @@ def tidy_path(path):
         if site != tidied[-1]:
             tidied.append(site)
     return tidied
+    
 
 class qcd_lattice():
     def __init__(self, Nx, Ny, Nt): 
@@ -69,7 +70,7 @@ class qcd_lattice():
 
         self.states = list(product(range(0, Nt), range(0, Nx), range(0, Ny)))
         
-        self.sites = dict((site, {}) for site in self.states)
+        self.sites = dict((site, []) for site in self.states)
         #sites = {site: {associated link: direction link points}}
 
         for i in range(len(self.states)):
@@ -79,13 +80,15 @@ class qcd_lattice():
                     nlinks += 1 
                     direction = locate_link_dir(ni, nj)
                     self.links[nlinks] = [ni, nj, direction, I3()]
-                    self.sites[ni].update({nlinks: direction})
-                    self.sites[nj].update({nlinks: direction})
-        
-       
-
+                    self.sites[ni].append(nlinks), self.sites[nj].append(nlinks)
 
         self.Uinit = np.kron(np.eye(nlinks), np.eye(3))
+
+    def get_shared_edge(self, n, m):
+        #m and n must be nearest neighbors!
+        n_edges, m_edges = self.sites[tuple(n)], self.sites[tuple(m)]
+        shared_edge = list(set(n_edges) & set(m_edges))[0]
+        return shared_edge
 
     def Plaquette(self, n, mu, nu):
         plq = np.eye(3)
@@ -105,51 +108,62 @@ class qcd_lattice():
 
         path = 1
         for ni, nj in zip(plq_sites, plq_sites[1:]):
-            ni_edges, nj_edges = self.sites[tuple(ni)], self.sites[tuple(nj)]
-            shared_edge = list(set(ni_edges) & set(nj_edges))[0]
+            shared_edge = self.get_shared_edge(ni, nj)
             if path == 1 or path == 2:
                 plq *= self.links[shared_edge][3] 
             elif path == 3 or path == 4:
-                plq *= self.links[shared_edge][3]
+                plq *= np.conjugate(self.links[shared_edge][3])
             
         return plq
 
     
     
-    def wilson_loop(self, n, R, j, t):
+    def wilson_loop(self, n, R, j, t, temp_gauge = False):
         #n: Initial Point
         #R: spatial distance between m and n 
         #j: Direction
         #t: Temporal distance (from t = 0 to t = t)
 
         loop = np.eye(3)
+        num_edges = 0
+
+        assert R < self.Nx, "Loop goes beyond bounds of the lattice"
+        assert t < self.Ny, "Loop goes beyond bounds of the lattice"
+        
 
         S = wilson_line_path(n, R, j)
         T = wilson_line_path(S[-1], t, 0)
         Sdag = wilson_line_path_dagger(S, t, 0)
         Tdag = wilson_line_path_dagger(T, -R, j)
-        
+
+        if temp_gauge == True: T, Tdag = np.eye(3), np.eye(3)
         
         path = tidy_path(S + T + Sdag + Tdag)
-        print(path)
+
+        for site_i in range(len(path) - 1):
+            shared_edge = self.get_shared_edge(path[site_i], path[site_i + 1])
+            num_edges += 1
+            if num_edges < R + t:
+                loop *= self.links[shared_edge][3]
+            else: loop *= np.conjugate(self.links[shared_edge][3])
+
         
-        return
+        return np.trace(loop)
 
 
 full_lattice = qcd_lattice(Nx, Ny, Nt)
 
-full_lattice.wilson_loop(n = (0, 0, 0), R = 3, j = 2, t = 2)
-
 def wilson_gauge_action(lattice):
     #Compute S_G(U) = 2/g^2 \sum_{n \in \Lambda} \sum_{\mu < \nu} Re tr[1 - U_mu,nu(n)]
     S_G = 0
+    beta = 6/(g**2)
 
     for n in lattice.states:
         for mu in (0, 1, 2):
             for nu in range(mu + 1, 3):
                 Uuv = lattice.Plaquette(n, mu, nu) #Plaquette
                 S_G += np.real(np.trace(np.eye(3) - Uuv))
-    S_G *= 2/(g**2)   
+    S_G *= beta
 
     return S_G
 
